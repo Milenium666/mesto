934597d509f2316f924846d8fdcf83c74f28a393
Milenium666 <whiteangel64rus@gmail.com>
2020-11-28 17:01:50 +0300

    Добавлен span для ошибок

---
+++ scripts/validate.js
@@ -0,0 +1,8 @@
+enableValidation({
+    formSelector: '.popup__data-container',
+    inputSelector: '.popup__data',
+    submitButtonSelector: '.popup__button-save',
+    // inactiveButtonClass: 'popup__button_disabled',
+    // inputErrorClass: 'popup__input_type_error',
+    // errorClass: 'popup__error_visible'
+  });

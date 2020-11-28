ffb4d0ca8ecebcc510c20f54510dd4946820dbe8
Milenium666 <whiteangel64rus@gmail.com>
2020-11-29 01:39:52 +0300

    настроена валидация но не до конца

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,22 +1,73 @@
-const formAdd = document.querySelector('.popup__data-container');
-const inputsList = formAdd.querySelectorAll('.popup__data');
+//Функция которая показывает ошибки
+function showError (form, input, config) {
+    const error = form.querySelector(`#${input.id}-error`);
+        error.textContent = input.validationMessage;
+        input.classList.add(config.inputInvalidClass);
+}
 
-formAdd.addEventListener('submit', (event) => {
-    event.preventDefault();
-});
+//функция раскрывает ошибку
+function hideError (form, input, config) {
+    const error = form.querySelector(`#${input.id}-error`);
+        error.textContent = '';
+        input.classList.remove(config.inputInvalidClass);
+}
 
-inputsList.forEach((input) => {
-    input.addEventListener('input', (event) => {
-        const error = formAdd.querySelector(`#${input.id}-error`);
-        error.textContent = input.validationMessange;
-    
-        if(input.validity.valid) {
-            input.classList.remove('popup__data_invalid');
-        } else {
-            input.classList.add('popup__data_invalid');
-        }
+//Функция проверяет валидность поля ввода
+function checkInputValidity (form, input, config) {
+    if(!input.validity.valid) {
+        showError(form, input, config);
+    } else {
+        hideError(form, input, config);
+    }
+}
+
+function setButtonState (button, isActive, config) {
+    if (isActive) {
+        button.classList.remove(config.buttonInvalidClass);
+        button.disabled = false;
+    } else {
+        button.classList.add(config.buttonInvalidClass);
+        button.disabled = true;
+    }
+
+}
+
+
+
+function setIventListeners (form, config) {
+
+    const inputsList = form.querySelectorAll(config.inputSelector);
+    const submitButton = form.querySelector(config.submitButtonSelector);
+    inputsList.forEach((input) => {
+        input.addEventListener('input', () => {
+            checkInputValidity (form, input, config);
+            setButtonState (submitButton, form.checkValidity(), config);
+        });
     });
-});
+}
 
+function enableValidation (config) {
+    const forms = document.querySelectorAll(config.formSelector);
+    forms.forEach((form) => {
+        setIventListeners(form, config);
 
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+        });
+
+        const submitButton = form.querySelector(config.submitButtonSelector);
+        setButtonState(submitButton, form.checkValidity(), config);
+    });
+
+}
+
+const validationConfig = {
+    formSelector: '.popup__data-container',
+    inputSelector: '.popup__data',
+    submitButtonSelector: '.popup__button-save',
+    buttonInvalidClass: 'popup__button-save_disabled',
+    inputInvalidClass: 'popup__data_invalid',
+}
+
+enableValidation(validationConfig);
 
